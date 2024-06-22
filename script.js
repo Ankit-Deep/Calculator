2c06eb7e6fa4f375dc8161b98eb508aa39d02610
Ankit Deep <AnkitDeep01@outlook.com>
2024-06-22 09:25:09 +0530

    Show input values properly

--- script.js
+++ script.js
@@ -1,40 +1,24 @@
-const outputValue = document.querySelector('#outputValue')
+const outputValue = document.querySelector('#outputValue');
 const icons = document.querySelector('.icons');
 const span = document.querySelectorAll('span');
+const inputValues = document.querySelector('#inputValues');
+let prev = inputValues.value;
 
 span.forEach(span => {
     span.addEventListener('click', (e)=> {
-        // console.log(e.target.innerText);
-        let value = e.target.innerText;
-        // console.log(value);
-
-        const inputValues = document.querySelector('#inputValues');
-
-        // if (inputValues.value === '') {
-        //     // inputValues.style.paddingLeft = '80%';
-        //     inputValues.value = value;
-        //     console.log(value);
-        // }
-        // let firstClick = value;
-        // console.log('firstclick',firstClick);
-
-        inputValues.value = value;
-        console.log(value);
-
-        // let concat = this.value + value; 
-        if (inputValues.value !== '') {
-            // inputValues.style.paddingLeft = '80%';
-            inputValues.value += value;
-            console.log(value);
-
-        }
-
-        // inputValues.style.position = 'relative';
-        // inputValues.style.paddingLeft = '85%';
-        // inputValues.value = value;
+        
+        let curr = e.target.innerText;
+        let show = prev + curr;
+        prev = show;
+        inputValues.value = show
 
 
+    });
+});
 
-
+document.querySelectorAll('.symbols').forEach(symbol => {
+    symbol.addEventListener('click', (e)=> {
+        console.log(e.target);
+        
     })
 })
